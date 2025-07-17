537a119f387cf2613f7c534de211f4398dda075a
Luca Tremamunno <tremaluca96@gmail.com>
2025-07-17 16:46:14 +0200

    Update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 plugins {
   jacoco
 
-  kotlin("jvm") version "1.9.25"
+  kotlin("jvm") version "2.2.0"
 
   id("org.jmailen.kotlinter") version "4.3.0"
   `maven-publish`
